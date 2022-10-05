3f35189c00923c6eaa224ecf72b2073965668c75
Maciob <macio99@gmail.com>
2022-10-05 15:31:39 +0200

    SUBNETS

--- compute.tf
+++ compute.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "MaciejBekasDemoEasy" {
     ami = "ami-0f540e9f488cfa27d"
     instance_type = "t2.micro"
-    availability_zone = "eu-west-2ą"
+    availability_zone = "eu-west-2a"
     # VPC
     subnet_id = "${aws_subnet.MY_SUBNETS["a"].id}"
     # Security Group
